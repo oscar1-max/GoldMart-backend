efd35f1899af511078142fba6bcfdc6cc06ebc2d
Oscar <akanegbuosinachi@gmail.com>
2026-08-11 10:21:03 +0100

    Add authentication routes in auth.js

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Authentication routes are working",
+  });
+});
+
+module.exports = router;
